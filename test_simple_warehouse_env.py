63219e098b81a54dac1c223a3a71d7478f57c551
Andrea Giuffrida <andrea.giuffrida@live.it>
2023-12-14 16:26:09 +0100

    Add csv writer | Add for loop for number of agents | Add total_steps, avg_steps, episode_length stats

--- test_simple_warehouse_env.py
+++ test_simple_warehouse_env.py
@@ -7,12 +7,25 @@ import mapf_gym_cap as mapf_gym
 import time
 from od_mstar3.col_set_addition import OutOfTimeError,NoSolutionError
 import random
+import csv
+import datetime
 
 results_path="primal_results"
 environment_path="saved_environments"
 warehouse_environment_path="warehouse_environments"
 if not os.path.exists(results_path):
     os.makedirs(results_path)
+
+def get_csv_logger(model_dir, default_model_name):
+    csv_path = os.path.join(model_dir, "log-"+default_model_name+".csv")
+    create_folders_if_necessary(csv_path)
+    csv_file = open(csv_path, "a")
+    return csv_file, csv.writer(csv_file)
+
+def create_folders_if_necessary(path):
+    dirname = os.path.dirname(path)
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname)
 
 class PRIMAL(object):
     '''
@@ -73,24 +86,38 @@ class PRIMAL(object):
         for agent in range(1,self.num_agents+1):
             action=np.argmax(policy_vec[agent-1])
             self.env._step((agent,action))
-          
+    
+    def compute_number_of_steps(self, timestep, prev_timestep, total_step):
+        '''
+        Computes the number of steps that the agent has taken
+        '''
+        for agent in range(self.num_agents):
+            if timestep[agent][0] != prev_timestep[agent][0] or timestep[agent][1] != prev_timestep[agent][1]:
+                total_step[agent] += 1
+
+
     def find_path(self,max_step=256):
         '''run a full environment to completion, or until max_step steps'''
         solution=[]
         step=0
+        agent_steps = np.zeros(self.num_agents)
         while((not self.env._complete()) and step<max_step):
             timestep=[]
             for agent in range(1,self.env.num_agents+1):
                 timestep.append(self.env.world.getPos(agent))
             solution.append(np.array(timestep))
             self.step_all_parallel()
+            if step > 0:
+                self.compute_number_of_steps(timestep, prev_timestep, agent_steps)
             step+=1
+            prev_timestep = timestep
             #print(step)
         if step==max_step:
             raise OutOfTimeError
         for agent in range(1,self.env.num_agents):
             timestep.append(self.env.world.getPos(agent))
-        return np.array(solution)
+        
+        return np.array(solution), np.array(agent_steps)
     
 def make_name(n,s,id,extension,dirname,extra=""):
     if extra=="":
@@ -124,10 +151,12 @@ def run_simulations(next,primal, verbose=False):
         # print('Starting test ({},{},{})'.format(n,s,id))
         max_time_step = 256 + 128*int(s>=80) + 128*int(s>=160)
         # max_time_step = 512
-        path=primal.find_path(max_time_step)
+        path, agent_steps = primal.find_path(max_time_step)
         results['finished']=True
         results['time']=time.time()-start_time
-        results['length']=len(path)
+        results['episode_length']=len(path)
+        results['total_steps'] = np.sum(agent_steps)
+        results['avg_steps'] = np.mean(agent_steps)
         if verbose:
             np.save(solution_filename,path)
     except OutOfTimeError:
@@ -176,30 +205,48 @@ if __name__ == "__main__":
     
     num_agents = 4
     size = 15
-    n_tests = 100
+    n_tests = 200
 
-    results = dict()
-    results['finished'] = []
-    results['time'] = []
-    results['length'] = []
-    results['crashed'] = []
-    print("Starting tests for %d agents" % num_agents)
+    date = datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S")
+    model_name = 'PRIMAL_simple_warehouse_' + date
+    csv_file, csv_logger = get_csv_logger(results_path, model_name)
 
-    for iter in range(n_tests):
-        print("Starting test %d" % iter)
-        res = run_simulations((num_agents,size,iter),primal)
-        print(res)
-        results['finished'].append(res['finished'])
-        results['time'].append(res['time'])
-        if res['finished']:
-            results['length'].append(res['length'])
-        results['crashed'].append(res['crashed'])
-    final_results = dict()
-    final_results['finished'] = np.sum(results['finished'])/len(results['finished'])
-    final_results['time'] = np.mean(results['time'])
-    final_results['length'] = np.mean(results['length'])
-    final_results['crashed'] = np.sum(results['crashed'])/len(results['crashed'])
-    print(final_results)    
+    for num_agents in [4, 8, 12, 16, 20, 22]:
+        results = dict()
+        results['finished'] = []
+        results['time'] = []
+        results['episode_length'] = []
+        results['total_steps'] = []
+        results['avg_steps'] = []
+        results['crashed'] = []
+        print("Starting tests for %d agents" % num_agents)
+
+        for iter in range(n_tests):
+            print("Starting test %d" % iter)
+            res = run_simulations((num_agents,size,iter),primal)
+            results['finished'].append(res['finished'])
+            results['time'].append(res['time'])
+            if res['finished']:
+                results['episode_length'].append(res['episode_length'])
+                results['total_steps'].append(res['total_steps'])
+                results['avg_steps'].append(res['avg_steps'])
+            results['crashed'].append(res['crashed'])
+
+        final_results = dict()
+        final_results['finished'] = np.sum(results['finished'])/len(results['finished'])
+        final_results['time'] = np.mean(results['time'])
+        final_results['episode_length'] = np.mean(results['episode_length'])
+        final_results['crashed'] = np.sum(results['crashed'])/len(results['crashed'])
+        final_results['total_steps'] = np.mean(results['total_steps'])
+        final_results['avg_steps'] = np.mean(results['avg_steps'])
+        print(final_results)
+
+        header = ["n_agents", "success_rate", "total_step", "avg_step", "episode_length"]
+        data = [num_agents, final_results['finished']*100, final_results['total_steps'], final_results['avg_steps'], final_results['episode_length']]
+        if num_agents == 4:
+            csv_logger.writerow(header)
+        csv_logger.writerow(data)
+        csv_file.flush()    
         
 
 print("finished all tests!")
