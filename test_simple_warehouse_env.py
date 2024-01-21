f2b22eaad26e6d96310c798dfa03debcf3165795
Andrea Giuffrida <andrea.giuffrida@live.it>
2024-01-21 12:25:13 +0100

    Works with 50x55 environment

--- test_simple_warehouse_env.py
+++ test_simple_warehouse_env.py
@@ -9,12 +9,26 @@ from od_mstar3.col_set_addition import OutOfTimeError,NoSolutionError
 import random
 import csv
 import datetime
+from tqdm import tqdm
 
 results_path="primal_results"
 environment_path="saved_environments"
 warehouse_environment_path="warehouse_environments"
 if not os.path.exists(results_path):
     os.makedirs(results_path)
+
+def count_collisions(solution):
+    collisions = 0
+    for timestep in range(len(solution)):
+        positions = solution[timestep]
+        for agent in range(len(solution[timestep])):
+            agent_pos = tuple(solution[timestep][agent])
+            positions_wo_agent = [tuple(pos) for pos in positions.copy()]
+            positions_wo_agent.pop(agent)
+            if agent_pos in positions_wo_agent:
+                collisions += 1        
+    return collisions
+
 
 def get_csv_logger(model_dir, default_model_name):
     csv_path = os.path.join(model_dir, "log-"+default_model_name+".csv")
@@ -125,17 +139,20 @@ def make_name(n,s,id,extension,dirname,extra=""):
     else:
         return dirname+'/'+"{}_agents_{}_size_{}_{}{}".format(n,s,id,extra,extension)
     
-def run_simulations(next,primal, verbose=False):
+def run_simulations(dataset_dir, map_name, next, primal, verbose=False):
     #txt file: planning time, crash, nsteps, finished
     (n,s,id) = next   # num_agents,size,density,iter
     # environment_data_filename=make_name(n,s,id,".npy",environment_path,extra="environment")
     # gym=mapf_gym.WarehouseEnv(num_agents=n)
 
     # pass world as argument
-    warehouse_env_path = os.path.join(warehouse_environment_path, 'simple_warehouse_env.npy')
-    simple_warehouse_world, open_list = np.load(warehouse_env_path, allow_pickle=True)  # loads world and open_list
+    warehouse_env_path = os.path.join(dataset_dir, map_name, "input/map/")
+    simple_warehouse_world = np.load(warehouse_env_path + map_name + ".npy")  # loads world
+
+    simple_warehouse_world[simple_warehouse_world == 1] = -1  # set obstacles to -1
+
     # generate agents start position and goals
-    world, goals = generate_start_and_goals(simple_warehouse_world, n, open_list)
+    world, goals = generate_start_and_goals(dataset_dir, map_name, simple_warehouse_world, n, id)
     gym = mapf_gym.WarehouseEnv(num_agents=n, world0=world, goals0=goals)
     primal.set_env(gym)
     solution_filename=make_name(n,s,id,".npy",results_path,extra="solution")
@@ -146,6 +163,7 @@ def run_simulations(next,primal, verbose=False):
     goals=tuple(gym.getGoals())
     start_time=time.time()
     results=dict()
+    solution = []
     start_time=time.time()
     try:
         # print('Starting test ({},{},{})'.format(n,s,id))
@@ -157,25 +175,55 @@ def run_simulations(next,primal, verbose=False):
         results['episode_length']=len(path)
         results['total_steps'] = np.sum(agent_steps)
         results['avg_steps'] = np.mean(agent_steps)
+        results['max_steps'] = np.max(agent_steps)
+        
+        if path is not None:
+            n_coll = count_collisions(path)
+            results['collisions'] = n_coll
+            results['crashed'] = n_coll > 0
+            results['coll_rate'] = n_coll / (results['episode_length'] * n)
+        else:
+            results['collisions'] = 0
+            results['crashed'] = False
+            results['coll_rate'] = 0.0
+
+        # Save solution
+        solution = [[] for i in range(num_agents)]
+        for i in range(num_agents):
+            # solution[i] = tuple(path[0][i]) + (0,)
+            for timestep in range(len(path)):
+                solution[i].append(tuple(path[timestep][i]) + (timestep,))
+
         if verbose:
             np.save(solution_filename,path)
     except OutOfTimeError:
         results['time']=time.time()-start_time
         results['finished']=False
-    results['crashed']=False
+        results['collisions'] = 0
+        results['crashed'] = False
+        results['coll_rate'] = 0.0
     if verbose:
         f=open(txt_filename,'w')
         f.write(json.dumps(results))
         f.close()
-    return results
+    
 
-def generate_start_and_goals(world, num_agents, open_list):
+
+    return results, solution
+
+def generate_start_and_goals(dataset_dir, map_name, world, num_agents, id):
+    
+    # Load case
+    filepath = os.path.join(dataset_dir, map_name, "input/start_and_goal", str(num_agents) + "_agents/")
+    case_name = filepath + map_name + "_" + str(num_agents) + "_agents_ID_" + str(id).zfill(5) + ".npy"
+    start_pos, goal_pos = np.load(case_name, allow_pickle=True)
+    
     #RANDOMIZE THE POSITIONS OF AGENTS
     agent_counter = 1
     agent_locations=[]
+
     while agent_counter<=num_agents:
-        agent_rand_pos = random.choice(open_list)
-        open_list.remove(agent_rand_pos)
+        agent_rand_pos = start_pos[agent_counter-1]
         x = agent_rand_pos[0]
         y = agent_rand_pos[1]
         if(world[x,y] == 0):
@@ -187,8 +235,7 @@ def generate_start_and_goals(world, num_agents, open_list):
     goals = np.zeros(world.shape).astype(int)
     goal_counter = 1   
     while goal_counter<=num_agents:
-        goal_rand_pos = random.choice(open_list)
-        open_list.remove(goal_rand_pos)
+        goal_rand_pos = goal_pos[goal_counter-1]
         x = goal_rand_pos[0]
         y = goal_rand_pos[1]
         if(goals[x,y]==0 and world[x,y]!=-1):
@@ -206,12 +253,26 @@ if __name__ == "__main__":
     num_agents = 4
     size = 15
     n_tests = 200
+    list_num_agents = [16, 32, 64, 128, 256]
+
+    dataset_dir = "/home/andrea/Thesis/baselines/Dataset"
+    map_name = "50_55_simple_warehouse"
+    model_save_name = "PRIMAL"
+
+    output_dir = os.path.join(dataset_dir, map_name, "output", model_save_name)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     date = datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%S")
     model_name = 'PRIMAL_simple_warehouse_' + date
     csv_file, csv_logger = get_csv_logger(results_path, model_name)
 
-    for num_agents in [4, 8, 12, 16, 20, 22]:
+    for num_agents in list_num_agents:
+        output_agent_dir = os.path.join(output_dir, str(num_agents) + "_agents/")
+        if not os.path.exists(output_agent_dir):
+            os.makedirs(output_agent_dir)
+
+
         results = dict()
         results['finished'] = []
         results['time'] = []
@@ -219,18 +280,29 @@ if __name__ == "__main__":
         results['total_steps'] = []
         results['avg_steps'] = []
         results['crashed'] = []
+        results['max_steps'] = []
+        results['collision_rate'] = []
         print("Starting tests for %d agents" % num_agents)
 
-        for iter in range(n_tests):
-            print("Starting test %d" % iter)
-            res = run_simulations((num_agents,size,iter),primal)
+        for iter in tqdm(range(n_tests)):
+            # print("Starting test %d" % iter)
+            res, solution = run_simulations(dataset_dir, map_name, (num_agents,size,iter),primal)
             results['finished'].append(res['finished'])
             results['time'].append(res['time'])
             if res['finished']:
                 results['episode_length'].append(res['episode_length'])
                 results['total_steps'].append(res['total_steps'])
                 results['avg_steps'].append(res['avg_steps'])
+                results['max_steps'].append(res['max_steps'])
             results['crashed'].append(res['crashed'])
+            results['collision_rate'].append(res['coll_rate'])
+
+        
+            # Save solution
+            solution_filepath = output_agent_dir + "solution_" + model_save_name + "_" + map_name + "_" + str(num_agents) + "_agents_ID_" + str(iter).zfill(5) + ".npy"
+            save_dict = {'metrics': res, 'solution': solution}
+            np.save(solution_filepath, save_dict)
+
 
         final_results = dict()
         final_results['finished'] = np.sum(results['finished'])/len(results['finished'])
@@ -239,14 +311,21 @@ if __name__ == "__main__":
         final_results['crashed'] = np.sum(results['crashed'])/len(results['crashed'])
         final_results['total_steps'] = np.mean(results['total_steps'])
         final_results['avg_steps'] = np.mean(results['avg_steps'])
+        final_results['max_steps'] = np.mean(results['max_steps'])
+        final_results['collision_rate'] = np.mean(results['collision_rate'])
         print(final_results)
 
-        header = ["n_agents", "success_rate", "total_step", "avg_step", "episode_length"]
-        data = [num_agents, final_results['finished']*100, final_results['total_steps'], final_results['avg_steps'], final_results['episode_length']]
+        header = ["n_agents", "success_rate", "collision_rate", "total_step", "avg_step", "max_step", "episode_length", "total_step_std", "avg_step_std", "max_step_std", "episode_length_std", "total_step_min", "avg_step_min", "max_step_min", "episode_length_min", "total_step_max", "avg_step_max", "max_step_max", "episode_length_max"]
+        data = [num_agents, final_results['finished']*100, final_results['collision_rate']*100, final_results['total_steps'], final_results['avg_steps'], final_results['max_steps'], final_results['episode_length'], np.std(results['total_steps']), np.std(results['avg_steps']), np.std(results['max_steps']), np.std(results['episode_length']), np.min(results['total_steps']), np.min(results['avg_steps']), np.min(results['max_steps']), np.min(results['episode_length']), np.max(results['total_steps']), np.max(results['avg_steps']), np.max(results['max_steps']), np.max(results['episode_length'])]
         if num_agents == 4:
             csv_logger.writerow(header)
         csv_logger.writerow(data)
         csv_file.flush()    
+
+        
+        
+        
+
         
 
 print("finished all tests!")
